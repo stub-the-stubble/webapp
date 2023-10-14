9f120cf22c81843e590bbd7473057c8ed5e22fbb
Archit Chandra <architchandra@gmail.com>
2023-10-14 22:49:09 +0530

    Add body content inside container, set up breakpoints, rename colours

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
-<div class="max-w-screen-sm min-h-screen flex flex-col gap-8 mx-auto place-content-center">
-    <h1 class="text-7xl text-stone-900">Stub the Stubble</h1>
-    <h2 class="text-2xl text-stone-900">
+<div class="container max-w-screen-sm min-h-screen flex flex-col gap-8 mx-auto place-content-center">
+    <h1 class="text-7xl text-brand-black">Stub the Stubble</h1>
+    <h2 class="text-2xl text-brand-black">
         Stub the Stubble is a platform to understand the problems and solutions related to stubble
         burning in India.
     </h2>
-    <p class="text-xl text-stone-900">Website coming soon</p>
+    <p class="text-xl text-brand-black">Website coming soon</p>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+
+
+const colors = require('tailwindcss/colors');
+
+
+
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            brand: {
+                white: '#ffffff',
+                black: colors.stone['900'],
+                'light-grey': colors.stone['50'],
+            },
+        },
+        container: {
+            center: true,
+            padding: '1.5rem',
+        },
+        screens: {
+            xs: '540px',
+            sm: '768px',
+            md: '1024px',
+            lg: '1280px',
+            xl: '1536px',
+        },
         extend: {}
     },
     plugins: []
