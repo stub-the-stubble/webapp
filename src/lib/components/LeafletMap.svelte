393e8b5090dca9d5fe95739fdd7d833333ca90a6
rndexe <himanshu.ernde@gmail.com>
2023-10-24 16:40:18 +0530

    Testing perf changes

--- src/lib/components/LeafletMap.svelte
+++ src/lib/components/LeafletMap.svelte
@@ -1,20 +1,21 @@
 <script>
     import { onMount } from 'svelte';
-    import { browser } from '$app/environment';
+    import 'leaflet/dist/leaflet.css';
 
     export let locations;
+    let mapElement;
 
-    if (browser) {
-        onMount(() => {
-            const map = L.map('map').setView([31.0, 76.5], 8);
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    onMount(async () => {
+        const {map, tileLayer, icon, marker } = await import('leaflet');
+            const l_map = map(mapElement).setView([31.0, 76.5], 8);
+            tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 minZoom: 8,
                 attribution:
                     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            }).addTo(map);
+            }).addTo(l_map);
 
-            const fire_icon = L.icon({
+            const fire_icon = icon({
                 iconUrl: 'red_dot.png',
                 iconSize: [15, 15], // size of the icon
                 iconAnchor: [7.5, 7.5], // point of the icon which will correspond to marker's location
@@ -22,30 +23,14 @@
             });
 
             locations.forEach((element) => {
-                const marker = L.marker([element.lat, element.long], { icon: fire_icon }).addTo(
-                    map
-                );
+                const marker = L.marker([element.lat, element.long], { icon: fire_icon }).addTo(l_map);
                 const marker_html = `District : ${element.dist} <br>
                            Time : ${element.time} <br>
                            FRP : ${element.frp} <br>`;
-                marker.bindPopup(marker_html, { closeButton: false }).openPopup();
+                marker.bindPopup(marker_html, { closeButton: false });
             });
-        });
-    }
+        
+    });
 </script>
 
-<svelte:head>
-    <link
-        rel="stylesheet"
-        href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-        crossorigin=""
-    />
-    <script
-        src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-        crossorigin=""
-    ></script>
-</svelte:head>
-
-<div id="map" class="w-full aspect-square sm:aspect-video mx-auto" />
+<div id="map" class="w-full aspect-square sm:aspect-video mx-auto" bind:this={mapElement} />
