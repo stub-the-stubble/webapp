83a7444d59b685725e3efcf6973dbf261297f22c
rndexe <himanshu.ernde@gmail.com>
2023-10-24 11:45:57 +0530

    Fix bar chart overflow

--- src/lib/components/DistrictBarchart.svelte
+++ src/lib/components/DistrictBarchart.svelte
@@ -4,7 +4,6 @@
     import { onMount } from 'svelte';
     import district_names_list from '$lib/data/district_names_list.json';
 
-
     export let data_array;
 
     let width = 800;
@@ -14,12 +13,14 @@
 
     // Prevent domain collapsing to midpoint if all values are zero
     if (Math.max(...xDomain) == 0) {
-        xDomain.push(1)
+        xDomain.push(1);
     }
+    const label_x = 200;
+    const gap_x = 15;
     const yScale = scaleBand().domain(district_names_list).range([0, height]).paddingInner(0.5);
     const xScale = scaleLinear()
         .domain([0, Math.max.apply(null, xDomain)])
-        .range([0, width - 150]);
+        .range([0, width - label_x - gap_x]);
 
     let show = false;
 
@@ -29,13 +30,16 @@
 </script>
 
 <div class=" basis-1/2">
-    <svg class="w-full h-auto overflow-visible fill-brand-black" viewBox={`0 0 ${width} ${height}`}>
+    <svg
+        class="w-full h-auto fill-brand-black"
+        viewBox={`0 0 ${width} ${height}`}
+    >
         <g class="fill-brand-black">
             {#each data_array as d, i}
                 <text
                     text-anchor="end"
-                    font-size="1.25em"
-                    x="145"
+                    class="text-xl"
+                    x={label_x}
                     dy="0.32em"
                     y={yScale(d[0]) + yScale.bandwidth() / 2}
                 >
@@ -44,7 +48,7 @@
                 {#if show}
                     <rect
                         in:slide={{ duration: 1000, axis: 'x' }}
-                        x={165}
+                        x={label_x + gap_x}
                         y={yScale(d[0])}
                         width={xScale(d[1])}
                         height={yScale.bandwidth()}
@@ -52,16 +56,17 @@
                 {/if}
                 {#if d[1] == 0}
                     <text
-                        text-anchor="end"
-                        x={xScale(d[1]) + 175}
+                        class="text-xl"
+                        text-anchor="start"
+                        x={xScale(d[1]) + label_x + gap_x}
                         dy="0.32em"
                         y={yScale(d[0]) + yScale.bandwidth() / 2}>0</text
                     >
                 {:else}
                     <text
-                        class="fill-brand-lightest-grey"
+                        class="fill-brand-lightest-grey text-xl"
                         text-anchor="end"
-                        x={xScale(d[1]) + 160}
+                        x={label_x + gap_x + xScale(d[1]) - 5}
                         dy="0.32em"
                         y={yScale(d[0]) + yScale.bandwidth() / 2}
                     >
