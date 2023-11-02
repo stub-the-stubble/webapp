be03fe5c4381458a0f71e1b39914079bc22392ee
rndexe <himanshu.ernde@gmail.com>
2023-11-02 19:22:31 +0530

    Scroll restoration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
     import '../app.css';
+    import { afterNavigate } from '$app/navigation';
+
+    afterNavigate(() => {
+        if (!scroll_restored) {
+            main_el.scrollTop = 0;
+        }
+        scroll_restored = false;
+    });
+
+    export const snapshot = {
+        capture() {
+            return main_el.scrollTop;
+        },
+        restore(scroll_top) {
+            main_el.scrollTop = scroll_top;
+
+            // Restore is not called for the first navigation to a page,
+            // use this flag to track whether to reset the scroll to top or not in afterNavigate
+            scroll_restored = true;
+        }
+    };
 </script>
 
 <slot />
