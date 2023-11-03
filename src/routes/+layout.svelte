044c63032d4e25dfddc0e3143ea67c8cb822830b
rndexe <himanshu.ernde@gmail.com>
2023-11-03 14:13:42 +0530

    Remove scroll snapshot

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,5 @@
 <script>
     import '../app.css';
-    import { afterNavigate } from '$app/navigation';
-
-    let scroll_restored = false;
-
-    afterNavigate(() => {
-        if (!scroll_restored) {
-            window.scrollY = 0;
-        }
-        scroll_restored = false;
-    });
-
-    export const snapshot = {
-        capture() {
-            return window.scrollY;
-        },
-        restore(scroll_top) {
-            window.scrollY = scroll_top;
-
-            // Restore is not called for the first navigation to a page,
-            // use this flag to track whether to reset the scroll to top or not in afterNavigate
-            scroll_restored = true;
-        }
-    };
 </script>
 
 <slot />
