0e11ffee9f24ff6291de7b598749ab4944054c92
Archit Chandra <architchandra@gmail.com>
2023-11-27 17:17:17 +0530

    Show chart data for previous 2 weeks - first pass

--- src/components/FireCountBarChart.svelte
+++ src/components/FireCountBarChart.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { scaleLinear, scaleTime, scaleBand } from 'd3-scale';
+    import { scaleLinear, scaleTime } from 'd3-scale';
     import { axisLeft, axisBottom } from 'd3-axis';
+    import { line, curveMonotoneX } from 'd3-shape';
     import { max, extent } from 'd3-array';
     import { select } from 'd3-selection';
     
@@ -8,8 +9,8 @@
     
     let svg;
     const dimensions = {
-        width: 1200,
-        height: 600,
+        width: 600,
+        height: 300,
         marginLeft: 40,
         marginRight: 40,
         marginTop: 30,
@@ -18,28 +19,30 @@
     
     $: if (data) {
         data = Object.keys(data).map((key) => [key, data[key]]);
-        // console.log(data);
-        
-        let data_filtered = data.slice(data.length - 5);
-        
-        console.log(data_filtered);
-        
-        let xScale = scaleTime()
-            .domain(extent(data_filtered, (d) => new Date(d[0])))
-            .range([dimensions.marginLeft, dimensions.width - dimensions.marginRight]);
-        
-        let yScale = scaleLinear()
-            .domain([0, max(data_filtered, (d) => d[1])])
-            .range([dimensions.height - dimensions.marginBottom, dimensions.marginTop]);
-            
-        let xBand = scaleBand()
-            .domain(data_filtered.map((d) => d[0]))
-            .range([dimensions.marginLeft, dimensions.width - dimensions.marginRight])
-            .paddingInner(0.1);
-            
+        let data_filtered = data.slice(data.length - 12).map((data) => [new Date(data[0]).setHours(0, 0, 0, 0), data[1]]);
         
         // Select the svg element
         let svgSelection = select(svg);
+        
+        let xScale = scaleTime()
+            .domain(extent(data_filtered, (d) => new Date(d[0]))).nice()
+            .range([dimensions.marginLeft, dimensions.width - dimensions.marginRight]);
+        
+        let yScale = scaleLinear()
+            .domain([0, max(data_filtered, (d) => d[1])]).nice()
+            .range([dimensions.height - dimensions.marginBottom, dimensions.marginTop]);
+            
+        // Create the line generator
+        const linePath = line()
+            .curve(curveMonotoneX)
+            .x((d, i, data) => {
+                console.log('x', d[0], ' - ', i, '-', data);
+                return xScale(new Date(d[0]))
+            })
+            .y((d, i, data) => {
+                console.log('y', d[1], ' - ', i, '-', data);
+                return yScale(d[1])
+            });
             
         // Add the x-axis and labels
         svgSelection.append('g')
@@ -51,19 +54,21 @@
             .attr('transform', `translate(${dimensions.marginLeft}, 0)`)
             .call(axisLeft(yScale).tickSizeOuter(0));
             
-        svgSelection.append('g')
-                .attr('fill', 'steelblue')
-            .selectAll()
+        svgSelection.selectAll('circle')
             .data(data_filtered)
-            .join('rect')
-                .attr('x', (d) => xScale(new Date(d[0]) - xBand.bandwidth()/2))
-                .attr('y', (d) => yScale(d[1]))
-                .attr('width', (d) => xBand.bandwidth())
-                .attr('height', (d) => yScale(0) - yScale(d[1]))
-                .attr('data-count', (d) => d[1]);
-        
+            .join(
+                (enter) => {
+                    return enter.append('circle')
+                        .attr('fill', 'red')
+                        .attr('r', 3)
+                        .attr('cx', (d) => xScale(new Date(d[0])))
+                        .attr('cy', (d) => yScale(d[1]))
+                }
+            );
     }
 </script>
+
+
 
 <svg class="w-full h-auto fill-black" viewBox={`0 0 ${dimensions.width} ${dimensions.height}`} bind:this={svg}>
 </svg>
