be59b6dac19d4d2d839a2b42a86f1b2c41ea73d1
Archit Chandra <architchandra@gmail.com>
2023-11-29 17:31:26 +0530

    Adjust footer link hover styling

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,6 @@
 <footer class="mb-8">
     <p class="text-sm xs:text-base px-6 text-left xs:text-center text-grey">
-        Stub the Stubble is a public interest project by <a
-            class="underline underline-offset-4"
-            href="https://www.saroja.earth"
-        >
+        Stub the Stubble is a public interest project by <a class="underline underline-offset-4 hover:text-brown" href="https://www.saroja.earth">
             Saroja Earth
         </a>
     </p>
