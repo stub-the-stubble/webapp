2c2fc162262f7791c127a6ba15e88d9510ff79b5
Archit Chandra <architchandra@gmail.com>
2023-11-30 18:22:19 +0530

    Adjust header nav, remove Saroja Earth link from header nav

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,6 @@
 <script>
     let primaryNavItems = [{
-        text: 'Home',
+        text: 'Home (All Data)',
         url: '/',
     },{
         text: 'Punjab',
@@ -8,16 +8,13 @@
     },{
         text: 'Haryana',
         url: '/haryana',
-    },{
+    }];
+    let secondaryNavItems = [{
         text: 'Insights',
         url: 'https://info.stubthestubble.com/insights/',
     },{
         text: 'Resources',
         url: 'https://info.stubthestubble.com/resources/',
-    }];
-    let secondaryNavItems = [{
-        text: 'About Saroja Earth',
-        url: 'https://www.saroja.earth/about/',
     }];
 </script>
 
@@ -25,11 +22,11 @@
 
 <div class="container my-4">
     <div class="w-full xl:w-5/6 mx-auto">
-        <nav class="flex justify-between pb-2 border-b border-light-grey overflow-x-auto">
+        <nav class="flex justify-between pb-2 border-b border-light-grey">
             {#if primaryNavItems.length}
                 <ul class="flex">
                     {#each primaryNavItems as item}
-                        <li class="mr-4 last:mr-0">
+                        <li class="flex-none mr-4 last:mr-0 hidden sm:block first:block">
                             <a href="{item.url}" class="hover:text-brown">
                                 {item.text}
                             </a>
@@ -38,9 +35,9 @@
                 </ul>
             {/if}
             {#if secondaryNavItems.length}
-                <ul class="hidden sm:flex">
+                <ul class="flex">
                     {#each secondaryNavItems as item}
-                        <li class="mr-4 last:mr-0">
+                        <li class="flex-none mr-4 last:mr-0">
                             <a href="{item.url}" class="hover:text-brown">
                                 {item.text}
                             </a>
