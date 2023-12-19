e172f72fd4d47142b8fa7e88f28eb1b85dd1b8b1
Archit Chandra <architchandra@gmail.com>
2023-12-19 15:31:44 +0530

    Fix heading levels on state pages

--- src/components/StateSummary.svelte
+++ src/components/StateSummary.svelte
@@ -1,14 +1,18 @@
 <script>
     import { LeafletMap, StateMap, CumulativeDataTable, FireCountChart, DistrictBarchart } from '$components';
+    import { page } from '$app/stores';
     import { states } from '$lib/data/site_info.js';
     import { fires_data } from '../stores/fires_data.js';
     import { IntersectionObserver } from '$lib/utils';
 
 
 
-    export let state, state_code, todays_data, districts_data, historical_data, layout;
+    export let state, state_code, todays_data, districts_data, historical_data, layout, isStatePage, headingLevel, subheadingLevel;
 
     state_code = states[state].code;
+    isStatePage = $page.params.state === state;
+    headingLevel = isStatePage ? 'h1' : 'h2';
+    subheadingLevel = isStatePage ? 'h2' : 'h3';
 
     $: {
         layout = layout ?? 'default';
@@ -25,11 +29,11 @@
 
 <div>
     <div class="my-12 xs:my-16">
-        <h2 class="mb-6 text-5xl text-brown font-bold uppercase">
+        <svelte:element this={headingLevel} class="mb-6 text-5xl text-brown font-bold uppercase">
             <a href={states[state].url} class="hover:underline">
                 {states[state].name}
             </a>
-        </h2>
+        </svelte:element>
         <CumulativeDataTable data={historical_data?.total.dates} last_updated={todays_data?.last_update} {layout} />
     </div>
     <div class="flex flex-col {layout === 'narrow' ? '' : 'md:flex-row'} md:items-center md:gap-16 mb-12 last:mb-0">
@@ -41,16 +45,16 @@
         <IntersectionObserver>
             {#if todays_data && todays_data.total}
                 <div class="mb-12 last:mb-0">
-                    <h3 class="mb-6 text-xl font-semibold capitalize">
+                    <svelte:element this={subheadingLevel} class="mb-6 text-xl font-semibold capitalize">
                         Districts with the most stubble fires
-                    </h3>
+                    </svelte:element>
                     <DistrictBarchart total_count={todays_data.total} {districts_data} />
                 </div>
             {/if}
             <div class="mb-12 last:mb-0">
-                <h3 class="mb-4 text-xl font-semibold capitalize">
+                <svelte:element this={subheadingLevel} class="mb-4 text-xl font-semibold capitalize">
                     Last 30 days' fire count
-                </h3>
+                </svelte:element>
                 <FireCountChart data={historical_data?.total.dates} />
             </div>
         </IntersectionObserver>
