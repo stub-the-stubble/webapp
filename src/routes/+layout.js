cf0c3e19b5f70728078e1b61b37fa529fe024224
Archit Chandra <architchandra@gmail.com>
2024-04-15 14:25:50 +0530

    Update Svelte to v4 and Sveltekit to v2

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -6,16 +6,16 @@ import { getDateISO } from '$lib/utils';
 export const prerender = true;
 export const trailingSlash = 'always';
 
-export function load({ fetch }) {
+export async function load({ fetch }) {
     //Get date string YYYY-MM-DD format in IST
     const currentDateStr = getDateISO();
 
     if (browser) {
         return {
-            PB_today: fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/PB/${currentDateStr}.json`).then((res) => res.json()),
-            PB_historical: fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/PB/historical_data.json`).then((res) => res.json()),
-            HR_today: fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/HR/${currentDateStr}.json`).then((res) => res.json()),
-            HR_historical: fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/HR/historical_data.json`).then((res) => res.json()),
+            PB_today: await fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/PB/${currentDateStr}.json`).then((res) => res.json()),
+            PB_historical: await fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/PB/historical_data.json`).then((res) => res.json()),
+            HR_today: await fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/HR/${currentDateStr}.json`).then((res) => res.json()),
+            HR_historical: await fetch(`https://stub-the-stubble.github.io/data-pipeline/v2/HR/historical_data.json`).then((res) => res.json()),
         };
     }
 }
