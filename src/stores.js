c82b0ab4546926309fbb4f4d56de37a4d805a364
Archit Chandra <architchandra@gmail.com>
2024-04-19 15:17:58 +0530

    Re-add date picker code

--- src/stores.js
+++ src/stores.js
@@ -5,5 +5,7 @@ import { writable } from 'svelte/store';
 // All fires data
 export const fires_data = writable(null);
 
-// Highlighted date
+// Dates for filtering data
+export const startDate = writable(null);
+export const endDate = writable(null);
 export const highlightedDate = writable(null);
