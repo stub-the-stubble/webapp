d7edc5a574fdfea39e8b5e075a58e0e0e60ea466
Archit Chandra <architchandra@gmail.com>
2024-04-19 15:12:39 +0530

    Temporarily remove datepicker code

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,4 @@
 <script>
-    import Datepicker from "$components/Datepicker/Datepicker.svelte";
-
     let primaryNavItems = [{
         text: 'Home (All Data)',
         url: '/',
@@ -48,6 +46,5 @@
                 </ul>
             {/if}
         </nav>
-        <Datepicker/>
     </div>
 </div>
